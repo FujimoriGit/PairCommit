573a96dae29bad23a082a2d203203b405ace5d61
Daiki Fujimori <G016C1102@outlook.jp>
2026-06-20 14:05:25 +0900

    Initial Commit

---
+++ PairCommit/PairCommitApp.swift
@@ -0,0 +1,18 @@
+//
+//  PairCommitApp.swift
+//  PairCommit
+//  
+//  Created by Daiki Fujimori on 2026/06/20
+//  
+
+
+import SwiftUI
+
+@main
+struct PairCommitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PairCommitTests/PairCommitTests.swift
@@ -0,0 +1,18 @@
+//
+//  PairCommitTests.swift
+//  PairCommitTests
+//  
+//  Created by Daiki Fujimori on 2026/06/20
+//  
+
+
+import Testing
+@testable import PairCommit
+
+struct PairCommitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
